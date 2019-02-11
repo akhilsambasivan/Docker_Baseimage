8669b5e0b51b6f264ec8e97ca203567374ce85b4
Akhil Sambasivan <akhil.sambasivan@gmail.com>
2019-02-11 19:42:54 +0000

    12-02-2019

---
+++ Source/rpm_install.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+rpm -ivh /Source/rpm/libgcj-4.4.7-23.el6.x86_64.rpm
+rpm -ivh /Source/rpm/oracle-instantclient11.2-basic-11.2.0.4.0-1.x86_64.rpm
+rpm -ivh /Source/rpm/oracle-instantclient11.2-devel-11.2.0.4.0-1.x86_64.rpm
+rpm -ivh /Source/rpm/oracle-instantclient11.2-jdbc-11.2.0.4.0-1.x86_64.rpm
+rpm -ivh /Source/rpm/oracle-instantclient11.2-odbc-11.2.0.4.0-1.x86_64.rpm
+rpm -ivh /Source/rpm/oracle-instantclient11.2-sqlplus-11.2.0.4.0-1.x86_64.rpm
+rpm -ivh /Source/rpm/oracle-instantclient11.2-tools-11.2.0.4.0-1.x86_64.rpm
+
